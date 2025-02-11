990f7b3732276d434058c1e79992e4687df9c874
NehalAs <nehalashraf104@gmail.com>
2025-02-11 16:45:23 +0200

    ddd

--- lib/modules/vistors_modules/reservation_successfully/presentation/views/reservation_successfully_view.dart
+++ lib/modules/vistors_modules/reservation_successfully/presentation/views/reservation_successfully_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:parotia/core/shared_components/custom_button.dart';
+import 'package:parotia/core/utils/app_util.dart';
+import 'package:parotia/modules/vistors_modules/visitor_home/presentation/views/visitor_home_view.dart';
 
 import '../../../../../core/utils/styles.dart';
 
@@ -8,10 +11,31 @@ class ReservationSuccessfullyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Reservation Successfully',style: Styles.fontSize18Bold,)
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              Text(
+                'Reservation Successfully',
+                style: Styles.fontSize18Bold,
+              ),
+              SizedBox(
+                width: 350,
+                child: Text(
+                  'Elementum nisl accumsan, urna ipsum sapien pulvinar. Est, eget pellentesque fermentum sed massa sit id iaculis vitae.',
+                  style: Styles.fontSize16RegularGrey,
+                ),
+              ),
+              const Spacer(),
+              CustomButton(text: 'Ok', onPressed: () {
+                AppUtil.removeUntilNavigator(context, const VisitorHomeView());
+              },),
+            ],
+          ),
+
       ),
     );
   }
